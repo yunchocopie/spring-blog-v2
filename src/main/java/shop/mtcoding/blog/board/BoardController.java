7a3b3a38ef3f84dc6c171488505ebd1f89d6c454
yunchocopie <yunnn687@naver.com>
2024-03-12 16:09:48 +0900

    게시글 수정 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -16,6 +16,21 @@ public class BoardController {
 
     private final BoardNativeRepository boardNativeRepository;
 
+    @PostMapping("/board/{id}/update")
+    public String update(@PathVariable Integer id, String title, String content, String username) {
+        boardNativeRepository.updateById(id, title, content, username);
+
+        return "redirect:/board/" + id;
+    }
+
+    @GetMapping("/board/{id}/update-form")
+    public String updateForm(@PathVariable Integer id, HttpServletRequest request) {
+        Board board = boardNativeRepository.findById(id);
+        request.setAttribute("board", board);
+
+        return "/board/update-form";
+    }
+
     @PostMapping("/board/{id}/delete")
     public String delete(@PathVariable Integer id) {
         boardNativeRepository.deleteByIc(id);
